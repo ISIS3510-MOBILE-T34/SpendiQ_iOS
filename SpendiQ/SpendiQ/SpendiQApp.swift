27c77861c6ae59d040a8a1b8c2dbd4b612e86f63
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-09-26 15:24:53 -0500

    new project + Firebase basic
    A new template for the project was set and the initial connection with fire base was established. The simulator runs and Firebase return true

---
+++ SpendiQ/SpendiQ/SpendiQApp.swift
@@ -0,0 +1,28 @@
+//
+//  SpendiQApp.swift
+//  SpendiQ
+//
+//  Created by Juan Salguero on 26/09/24.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct SpendiQApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("Firebase good")
+    return true
+  }
+}
---
+++ SpendiQ/SpendiQTests/SpendiQTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpendiQTests.swift
+//  SpendiQTests
+//
+//  Created by Juan Salguero on 26/09/24.
+//
+
+import Testing
+@testable import SpendiQ
+
+struct SpendiQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
