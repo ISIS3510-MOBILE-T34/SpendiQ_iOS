09acd0f6793277c557ba9ae2d104db94ad5d3d72
Daniel Clavijo <d.clavijog@uniandes.edu.co>
2024-10-02 20:10:32 -0500

    Commun fixes

--- SpendiQ/SpendiQ/Authentication/AuthenticationService.swift
+++ SpendiQ/SpendiQ/Authentication/AuthenticationService.swift
@@ -4,27 +4,87 @@
 //
 //  Created by Daniel Clavijo on 30/09/24.
 //
-
 import Foundation
 import Combine
+import FirebaseAuth
 
 protocol AuthenticationServiceProtocol {
     func login(email: String, password: String) -> AnyPublisher<Bool, Error>
     func signUp(email: String, password: String) -> AnyPublisher<Bool, Error>
+    func resetPassword(email: String) -> AnyPublisher<Void, Error>
+    func verifyResetCode(code: String) -> AnyPublisher<Void, Error>
+    func confirmPasswordReset(code: String, newPassword: String) -> AnyPublisher<Void, Error>
 }
 
 class AuthenticationService: AuthenticationServiceProtocol {
     func login(email: String, password: String) -> AnyPublisher<Bool, Error> {
-        // TODO: Implement actual login logic with Firebase
-        return Just(true)
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+        Deferred {
+            Future { promise in
+                Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(true))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
     }
     
     func signUp(email: String, password: String) -> AnyPublisher<Bool, Error> {
-        // TODO: Implement actual sign up logic with Firebase
-        return Just(true)
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
+        Deferred {
+            Future { promise in
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(true))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func resetPassword(email: String) -> AnyPublisher<Void, Error> {
+        Deferred {
+            Future { promise in
+                Auth.auth().sendPasswordReset(withEmail: email) { error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(()))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func verifyResetCode(code: String) -> AnyPublisher<Void, Error> {
+        Deferred {
+            Future { promise in
+                // El método devuelve (String?, Error?), ambos deben ser manejados
+                Auth.auth().verifyPasswordResetCode(code) { result, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(()))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func confirmPasswordReset(code: String, newPassword: String) -> AnyPublisher<Void, Error> {
+        Deferred {
+            Future { promise in
+                Auth.auth().confirmPasswordReset(withCode: code, newPassword: newPassword) { error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(()))
+                    }
+                }
+            }
+        }.eraseToAnyPublisher()
     }
 }
