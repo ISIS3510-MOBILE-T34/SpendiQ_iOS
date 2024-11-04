d164af91c90a870c6405ab7ae076d68aaa8b0ad9
Juan David Salguero Medina <j.salguero@uniandes.edu.co>
2024-11-04 10:48:07 -0500

    Access only to user accounts

--- SpendiQ/SpendiQ/ViewModel/BankAccountViewModel.swift
+++ SpendiQ/SpendiQ/ViewModel/BankAccountViewModel.swift
@@ -1,32 +1,45 @@
 // BankAccountViewModel.swift
 
 import FirebaseFirestore
+import FirebaseAuth
 
 class BankAccountViewModel: ObservableObject {
     @Published var accounts: [BankAccount] = []
     private let db = Firestore.firestore()
     
+    // Adds an account to firebase collection ("userID/accounts")
     func addAccount(name: String, amount: Double) {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("Usuario no autenticado")
+            return
+        }
+
         let newAccount = BankAccount(id: nil, name: name, amount: amount)
-        
+
         do {
-            let _ = try db.collection("accounts").addDocument(from: newAccount) { error in
+            let _ = try db.collection("users").document(userId).collection("accounts").addDocument(from: newAccount) { error in
                 if let error = error {
-                    print("Error saving account: \(error.localizedDescription)")
+                    print("Error al guardar la cuenta: \(error.localizedDescription)")
                 } else {
-                    print("Account saved successfully")
+                    print("Cuenta guardada exitosamente")
                     self.getBankAccounts()
                 }
             }
         } catch {
-            print("Error saving account: \(error.localizedDescription)")
+            print("Error al guardar la cuenta: \(error.localizedDescription)")
         }
     }
     
+    // gets all accounts from ("userID/accounts")
     func getBankAccounts() {
-        db.collection("accounts").getDocuments { (querySnapshot, error) in
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("Usuario no autenticado")
+            return
+        }
+
+        db.collection("users").document(userId).collection("accounts").getDocuments { (querySnapshot, error) in
             if let error = error {
-                print("Error retrieving accounts: \(error.localizedDescription)")
+                print("Error al recuperar las cuentas: \(error.localizedDescription)")
             } else {
                 let accounts = querySnapshot?.documents.compactMap { document -> BankAccount? in
                     var account = try? document.data(as: BankAccount.self)
@@ -35,49 +48,67 @@ class BankAccountViewModel: ObservableObject {
                 }
                 DispatchQueue.main.async {
                     self.accounts = accounts ?? []
-                    print("Loaded accounts: \(self.accounts)")
+                    print("Cuentas cargadas: \(self.accounts)")
                 }
             }
         }
     }
     
+    // delete a given account("userID/accounts/accountID")
     func deleteAccount(accountID: String) {
-        db.collection("accounts").document(accountID).delete { error in
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("Usuario no autenticado")
+            return
+        }
+
+        db.collection("users").document(userId).collection("accounts").document(accountID).delete { error in
             if let error = error {
-                print("Error deleting account: \(error.localizedDescription)")
+                print("Error al eliminar la cuenta: \(error.localizedDescription)")
             } else {
-                print("Account deleted successfully")
+                print("Cuenta eliminada exitosamente")
                 self.getBankAccounts()
             }
         }
     }
     
+    // update a given account("userID/accounts/accountID")
     func updateAccount(accountID: String, newName: String, newBalance: Double) {
-        db.collection("accounts").document(accountID).updateData([
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("Usuario no autenticado")
+            return
+        }
+
+        db.collection("users").document(userId).collection("accounts").document(accountID).updateData([
             "name": newName,
             "amount": newBalance
         ]) { error in
             if let error = error {
-                print("Error updating account: \(error.localizedDescription)")
+                print("Error al actualizar la cuenta: \(error.localizedDescription)")
             } else {
-                print("Account updated successfully")
+                print("Cuenta actualizada exitosamente")
                 self.getBankAccounts()
             }
         }
     }
     
+    // updates de balance of a given account("userID/accounts/accountID")
     func updateAccountBalance(accountID: String, amountChange: Double) {
-        db.collection("accounts").document(accountID).getDocument { (document, error) in
+        guard let userId = Auth.auth().currentUser?.uid else {
+            print("Usuario no autenticado")
+            return
+        }
+
+        db.collection("users").document(userId).collection("accounts").document(accountID).getDocument { (document, error) in
             if let document = document, document.exists {
                 let currentBalance = document.data()?["amount"] as? Double ?? 0.0
                 let newBalance = currentBalance + amountChange
-                self.db.collection("accounts").document(accountID).updateData([
+                self.db.collection("users").document(userId).collection("accounts").document(accountID).updateData([
                     "amount": newBalance
                 ]) { error in
                     if let error = error {
-                        print("Error updating account balance: \(error.localizedDescription)")
+                        print("Error al actualizar el saldo de la cuenta: \(error.localizedDescription)")
                     } else {
-                        print("Account balance updated")
+                        print("Saldo de la cuenta actualizado")
                         self.getBankAccounts()
                     }
                 }
