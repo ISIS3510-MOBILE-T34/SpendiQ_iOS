1b0a2637c39f1e87a2fa3779a8901a854d57f145
Julián Mora <j.morav@uniandes.edu.co>
2024-11-21 19:42:59 -0500

    New Feature and BQ Type 5

--- SpendiQ/SpendiQ/Model/OfferModel.swift
+++ SpendiQ/SpendiQ/Model/OfferModel.swift
@@ -18,4 +18,6 @@ struct Offer: Identifiable, Equatable {
     var longitude: Double // Longitude for map
     var distance: Int // Distance from the user, as an integer
     var shop: String //path to the related shop, which is in a path like this in Firestore (/shops/[id])
+    var featured: Bool // Sprint 4 - New feature for CAS: Can highlight one or more offers to show them first in the list and with a special design
+    var viewCount: Int32 // Sprint 4 - Alonso - New BQ type 4: Giving info to the shops about how many people were engaged by the offers, specially for the featured ones, where they need to pay an extra.
 }
